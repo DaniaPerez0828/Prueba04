7fadb0ed7a97dfc515b1259b4fad68006882600e
Dannia Perez <danniaperezri2808@gmail.com>
2024-10-07 00:40:02 -0600

    Crear clase Estudiante

---
+++ ControlAsist/Estudiante.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ControlAsistencia
+{
+    public class Estudiante
+    {
+        public string Nombre { get; private set; }
+        public int SesionesTotales { get; private set; }
+        public int SesionesAsistidas { get; private set; }
+
+        public Estudiante(string nombre, int sesionesTotales)
+        {
+            Nombre = nombre;
+            SesionesTotales = sesionesTotales;
+            SesionesAsistidas = 0;
+        }
+
+        public void RegistrarAsistencia(int asistencias)
+        {
+            SesionesAsistidas += asistencias;
+            if (SesionesAsistidas > SesionesTotales)
+            {
+                SesionesAsistidas = SesionesTotales; // No puede asistir más de lo total
+            }
+        }
+    }
+}
