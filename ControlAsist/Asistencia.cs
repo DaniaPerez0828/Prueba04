47a0473b88a0421784763431c7b85fae97985312
Dannia Perez <danniaperezri2808@gmail.com>
2024-10-07 08:31:02 -0600

    Se declaro una constante privada llamada Minimo_asistencia de 75

--- ControlAsist/Asistencia.cs
+++ ControlAsist/Asistencia.cs
@@ -7,17 +7,6 @@ namespace ControlAsistencia
     {
         private const double MINIMO_ASISTENCIA = 75;
 
-        public static double CalcularPorcentaje(int asistidas, int totales)
-        {
-            if (totales == 0)
-                return 0;
-            return (double)asistidas / totales * 100;
-        }
-
-        public static bool CumpleMinimo(int asistidas, int totales)
-        {
-            double porcentaje = CalcularPorcentaje(asistidas, totales);
-            return porcentaje >= MINIMO_ASISTENCIA;
         }
     }
 }
