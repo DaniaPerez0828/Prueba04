e3ea632c82e3c6b66c3378e667e31fe64a3554e0
Dannia Perez <danniaperezri2808@gmail.com>
2024-10-07 10:54:32 -0600

    Se crea un metodo constructor de la clase estudiante y se establecen los parametros (nombre y sesionestotales)

--- ControlAsist/Estudiante.cs
+++ ControlAsist/Estudiante.cs
@@ -7,5 +7,12 @@ namespace ControlAsistencia
         public string Nombre { get; private set; }
         public int SesionesTotales { get; private set; }
         public int SesionesAsistidas { get; private set; }
+
+        public Estudiante(string nombre, int sesionesTotales)
+        {
+            Nombre = nombre;
+            SesionesTotales = sesionesTotales;
+            SesionesAsistidas = 0;
+        }
     }
 }
