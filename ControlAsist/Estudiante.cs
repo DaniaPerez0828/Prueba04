257b6b91bf5721513d3b798c0de8f8e0223a4cb0
Dannia Perez <danniaperezri2808@gmail.com>
2024-10-07 10:52:09 -0600

    Creacion de las propiedades de la clase estudiante

--- ControlAsist/Estudiante.cs
+++ ControlAsist/Estudiante.cs
@@ -7,21 +7,5 @@ namespace ControlAsistencia
         public string Nombre { get; private set; }
         public int SesionesTotales { get; private set; }
         public int SesionesAsistidas { get; private set; }
-
-        public Estudiante(string nombre, int sesionesTotales)
-        {
-            Nombre = nombre;
-            SesionesTotales = sesionesTotales;
-            SesionesAsistidas = 0;
-        }
-
-        public void RegistrarAsistencia(int asistencias)
-        {
-            SesionesAsistidas += asistencias;
-            if (SesionesAsistidas > SesionesTotales)
-            {
-                SesionesAsistidas = SesionesTotales; // No puede asistir más de lo total
-            }
-        }
     }
 }
